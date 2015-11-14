d148168007d70ad868ff061ab9d555a4a238d34c
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2015-11-14 12:07:14 -0600

    Adds randomized word getters for constructing sentences

--- app/models/chain.rb
+++ app/models/chain.rb
@@ -1,4 +1,20 @@
 class Chain < ActiveRecord::Base
   validates :word, :uniqueness => { :scope => :previous }
 
+  def self.get_random_word
+    min_id = min
+    where('id >= ?', min_id + rand(max - min_id)).first
+  end
+
+  def self.get_random_next_word(word)
+    find where(:previous => word).pluck(:id).sample
+  end
+
+  private
+  def self.min
+    minimum(:id).to_i
+  end
+  def self.max
+    maximum(:id).to_i
+  end
 end
--- test/models/chain_test.rb
+++ test/models/chain_test.rb
@@ -1,18 +1,50 @@
 require 'test_helper'
 
 class ChainTest < ActiveSupport::TestCase
+
   test "get next word" do
     Chain.create :word=>'dog', :previous=>'the'
     assert_equal 'dog', Chain.find_by(:previous=>'the').word
   end
+
   test "unique entries" do
     assert Chain.create(:word=>'dog', :previous=>'the').save
     assert ! Chain.create(:word=>'dog', :previous=>'the').save
   end
+
   test "punctuation" do
     Chain.create :word=>',', :is_punct => true
     Chain.create :word=>'.', :is_punct => true, :is_end => true
     assert Chain.find_by(:word=>[',', '.']).is_punct? 
     assert Chain.find_by(:word=>'.').is_end? 
+  end
+
+  def Chain.rand(max)
+    ChainTest.random_values.shift
+  end
+  def self.random_values
+    @@random_values
+  end
+  test "random word" do
+    @@random_values = [2,0]
+    %w(dog cat mouse horse).each {|word|
+      Chain.create :word=>word, :previous=>'the'
+    }
+    assert_equal 'mouse', Chain.get_random_word.word
+    assert_equal 'dog', Chain.get_random_word.word
+  end
+
+  class ::Array
+    def sample
+      self[ChainTest.random_values.shift]
+    end
+  end
+  test "random next word" do
+    @@random_values = [2,0]
+    %w(dog cat mouse horse).each {|word|
+      Chain.create(:word=>word, :previous=>'the').save
+    }
+    assert_equal 'mouse', Chain.get_random_next_word('the').word
+    assert_equal 'dog', Chain.get_random_next_word('the').word
   end
 end
