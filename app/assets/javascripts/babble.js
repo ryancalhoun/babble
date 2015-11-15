5907cc1eebc38991b2ce8de3ecebc2c6dcf9887f
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2015-11-14 18:01:44 -0600

    Wired up javascript to callback on the controller

--- app/assets/javascripts/babble.js
+++ app/assets/javascripts/babble.js
@@ -1,1 +1,42 @@
-$('#history').text("hey");
+(function () {
+
+  var updateHistory = function(who, text) {
+    var history = $('#history');
+    history.append(who + ": " + text + "\n");
+    if(history.length)
+      history.scrollTop(history[0].scrollHeight - history.height());
+  };
+
+  var getSentence = function() {
+    $.ajax({
+      type: 'GET',
+      url: '/sentence'
+    }).done(function (text) {
+      updateHistory("Babble", text);
+    });
+  };
+
+  var sendText = function(text) {
+    $.ajax({
+      type: 'POST',
+      url: '/sentence',
+      data: {text: text}
+    }).done(function() {
+      getSentence();
+    });
+  };
+
+  getSentence();
+
+  $('#entry').keyup(function(e) {
+    if(e.keyCode == 13)
+    {
+      var text = $(this).val();
+      $(this).val('');
+
+      updateHistory("Me", text);
+      sendText(text);
+    }
+  });  
+
+}) ();
